2a71de9e5b9139b72953b8e6849c21a3e522b015
unknown <you@example.com>
2025-10-12 12:10:25 +0700

    Initial commit or update all files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace khmer25.com_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
